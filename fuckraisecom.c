611c93aa2540e579b369d53d862792b1887ecff4
Zhiyuan Wan <h@iloli.bid>
2021-12-09 01:26:52 +0800

    Do that gently

--- fuckraisecom.c
+++ fuckraisecom.c
@@ -5,8 +5,8 @@
 #include <openssl/buffer.h>
 #include <string.h>
 
-char ios_license_key[]="Venusutm";
-char ios_license_iv[] ="licenses";
+char ios_license_key[] = "Venusutm";
+char ios_license_iv[] = "licenses";
 
 char *base64_encode(const uint8_t *input, int length)
 {
@@ -32,9 +32,9 @@ char *base64_encode(const uint8_t *input, int length)
 }
 uint8_t *base64_decode(unsigned char *input, int length)
 {
-    BIO * b64 = NULL;
-    BIO * bmem = NULL;
-    uint8_t * buffer = (char *)malloc(length);
+    BIO *b64 = NULL;
+    BIO *bmem = NULL;
+    uint8_t *buffer = (char *)malloc(length);
     memset(buffer, 0, length);
 
     b64 = BIO_new(BIO_f_base64());
@@ -63,10 +63,10 @@ void decrypt_buf(char *in, char *out, int len)
 
 int main(int argc, char *argv[])
 {
-    char *hpass = argv[1];//"5ZMfsRFY61KWtNkv0KQrUg0MLY8Klt8iyZr0ykghgayovi9a4VxqRtDnwqz26py";
-                          // BJPqJJx+U9b8x11T1H9fzkwIKPpe2IYf0nvSw1WtQQhPqDyPOyJNZCdHbb7j6n+
-                          // 4kk2i75d28Mrmhv+fyTgUYFUpsD+TK0baeaKJjDTZFNDSSs0YwuqYpkBli6cSsh
-    char *lpass = argv[2];//"fMhlEDRRuxWTPtyk3ELemzJoKgSSNsKLP216Gw0ttrIBr0s8dcZmyyWkBC2n3I/";
+    char *hpass = argv[1]; //"5ZMfsRFY61KWtNkv0KQrUg0MLY8Klt8iyZr0ykghgayovi9a4VxqRtDnwqz26py";
+                           // BJPqJJx+U9b8x11T1H9fzkwIKPpe2IYf0nvSw1WtQQhPqDyPOyJNZCdHbb7j6n+
+                           // 4kk2i75d28Mrmhv+fyTgUYFUpsD+TK0baeaKJjDTZFNDSSs0YwuqYpkBli6cSsh
+    char *lpass = argv[2]; //"fMhlEDRRuxWTPtyk3ELemzJoKgSSNsKLP216Gw0ttrIBr0s8dcZmyyWkBC2n3I/";
 
     char *hpass_p;
     char *lpass_p;
@@ -74,16 +74,16 @@ int main(int argc, char *argv[])
     char dec_hpass[48];
     char dec_lpass[48];
     char allpass[128];
-	int i, len;
+    int i, len;
 
-	if(argc == 1)
-	{
-		printf("Usage: %s <hpass> <lpass>\n", argv[0]);
-		printf("Usage: %s <encpass>\n", argv[0]);
-		return -1;
-	}
-	else if(argc == 3)
-	{
+    if (argc == 1)
+    {
+        printf("Usage: %s <hpass> <lpass>\n", argv[0]);
+        printf("Usage: %s <encpass>\n", argv[0]);
+        return -1;
+    }
+    else if (argc == 3)
+    {
 
         hpass_p = base64_decode(hpass, strlen(hpass));
         lpass_p = base64_decode(lpass, strlen(hpass));
@@ -93,33 +93,32 @@ int main(int argc, char *argv[])
 
         len = strlen(dec_hpass) + strlen(dec_lpass);
         int j = 0, k = 0;
-        for(i = 0; i < len; i++)
-            if ( (i & 1) != 0 )
+        for (i = 0; i < len; i++)
+            if ((i & 1) != 0)
                 allpass[i] = dec_lpass[j++];
             else
                 allpass[i] = dec_hpass[k++];
 
         allpass[i] = 0;
-	}
-	else
-	{
-		memset(allpass, 0x00, 128);
-		strncpy(allpass, argv[1], 120);
-		len = strlen(allpass);
-        if(len == 63)
+    }
+    else
+    {
+        memset(allpass, 0x00, 128);
+        strncpy(allpass, argv[1], 120);
+        len = strlen(allpass);
+        if (len == 63)
         {
             lpass_p = base64_decode(argv[1], len);
             decrypt_buf(lpass_p, allpass, 48);
             printf("password is '%s'\n", allpass);
-        	return 0;
+            return 0;
         }
-        
-	}
+    }
 
-    for ( i = 0; i < len; i++ )
+    for (i = 0; i < len; i++)
         allpass[i] = allpass[i] - i - 5;
     //fwrite(allpass, 1, 96, fp);
     allpass[i] = 0;
     printf("password is '%s'\n", allpass);
-	return 0;
+    return 0;
 }
