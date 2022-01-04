27a055ac7c8fc174119a1fdd880811239693e395
Zhiyuan Wan <h@iloli.bid>
2022-01-04 13:32:36 +0800

    Update tty.sh

--- tty.sh
+++ tty.sh
@@ -6,21 +6,4 @@ if [[ $TTY == /dev/tty* ]]; then
         echo "You've login via a Serial/Modem Console."
         export TMOUT=300
         echo "The shell will automatically logout after "$TMOUT" secs."
-        fflush
-        stty -echo
-
-        sleep 1.5
-        echo -ne +++
-        sleep 1.5
-        echo ATS2=128
-        read -t 0.1 -N 16 discard #Modem 开着回显，要读两次
-        read -t 0.1 -N 16 discard
-        sleep 0.3
-        echo ATO0
-        read -t 0.1 -N 16 discard
-        read -t 0.1 -N 16 discard
-        sleep 0.2
-        read -t 0.1 -N 128 discard
-        stty echo
-        echo -ne '\b\b\b'
 fi
